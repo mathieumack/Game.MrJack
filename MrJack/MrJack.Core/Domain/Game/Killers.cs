ea5fb05e6d105969da5c79aa2b8ddf8191a1b033
Frédéric <fpinaud17@gmail.com>
2018-03-14 16:28:10 +0100

    [add] Add killers enum name

--- MrJack/MrJack.Core/Domain/Game/Killers.cs
+++ MrJack/MrJack.Core/Domain/Game/Killers.cs
@@ -3,7 +3,17 @@
     public enum Killers
     {
         None,
-        Butcher
+        Butcher,
+        William_Gull,
+        John_Smith,
+        Jeremy_Bert,
+        Joseph_Lane,
+        Miss_Stealthy,
+        Insp_Lestrade,
+        Sgt_Goodley,
+        Madame,
+        John_Pizzer
+
         // Define other
     }
 }
