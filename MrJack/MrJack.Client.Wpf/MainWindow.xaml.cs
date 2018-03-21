d07bd213ae85ae30947289abe34006afd969f33f
Rabatel <loris.rabatel@epsi.fr>
2018-03-21 16:37:10 +0100

    Valider

--- MrJack/MrJack.Client.Wpf/MainWindow.xaml.cs
+++ MrJack/MrJack.Client.Wpf/MainWindow.xaml.cs
@@ -1,20 +1,12 @@
 ﻿using MahApps.Metro.Controls;
-using MrJack.Core.Interfaces;
+using MrJack.Client.Wpf.Controls;
+using MrJack.Client.Wpf.Moqs;
+using MrJack.Core.Domain.Game;
 using MrJack.Core.Interfaces.Game;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MrJack.Client.Wpf
 {
@@ -24,6 +16,7 @@ namespace MrJack.Client.Wpf
     public partial class MainWindow : MetroWindow
     {
         IGame currentGame;
+        CardRender[,] cards;
 
         public MainWindow()
         {
@@ -35,6 +28,39 @@ namespace MrJack.Client.Wpf
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             newGameFlyout.IsOpen = true;
+
+            // TODO : Init game object
+            currentGame = new MoqsGame();
+
+            // Register CardRender table :
+            cards = new CardRender[5, 5];
+            cards[0, 0] = Card00; // Line 0
+            cards[0, 1] = Card01;
+            cards[0, 2] = Card02;
+            cards[0, 3] = Card03;
+            cards[0, 4] = Card04;
+            cards[1, 0] = Card10; // Line 1
+            cards[1, 1] = Card11;
+            cards[1, 2] = Card12;
+            cards[1, 3] = Card13;
+            cards[1, 4] = Card14;
+            cards[2, 0] = Card20; // Line 2
+            cards[2, 1] = Card21;
+            cards[2, 2] = Card22;
+            cards[2, 3] = Card23;
+            cards[2, 4] = Card24;
+            cards[3, 0] = Card30; // Line 3
+            cards[3, 1] = Card31;
+            cards[3, 2] = Card32;
+            cards[3, 3] = Card33;
+            cards[3, 4] = Card34;
+            cards[4, 0] = Card40; // Line 4
+            cards[4, 1] = Card41;
+            cards[4, 2] = Card42;
+            cards[4, 3] = Card43;
+            cards[4, 4] = Card44;
+
+            Refresh();
         }
 
         private void ConfigureNewGame_Click(object sender, RoutedEventArgs e)
@@ -44,7 +70,118 @@ namespace MrJack.Client.Wpf
 
         private void StartNewGame_Click(object sender, RoutedEventArgs e)
         {
-            currentGame.StartNewGame(Core.Domain.Game.PlayerType.MrJack, Core.Domain.Game.Difficulty.Easy);
+            currentGame.StartNewGame(PlayerType.MrJack, Difficulty.Easy);
+            Refresh();
         }
+
+        #region Refresh
+
+        private void Refresh()
+        {
+            if (currentGame == null)
+                return;
+
+            // Start to refresh available actions :
+            for (int i = 0; i < 4; i++)
+            {
+                var action = currentGame.AvailableActions[i];
+                var button = GetAvailableActionButton(i+1);
+                var image = GetAvailableActionImage(i+1);
+
+                button.IsEnabled = action.Selectable;
+            }
+
+            // Killer image type:
+            // We manage the Token image source :
+            switch (currentGame.Killer)
+            {
+                case Killers.None:
+                    KillerUserImage.Visibility = Visibility.Collapsed;
+                    break;
+                case Killers.Insp_Lestrade:
+                    KillerUserImage.Source = new BitmapImage(new Uri("/Images/killers/insplestrade.png", UriKind.Relative));
+                    break;
+                case Killers.Jeremy_Bert:
+                    KillerUserImage.Source = new BitmapImage(new Uri("/Images/killers/jeremybert.png", UriKind.Relative));
+                    break;
+                case Killers.John_Pizzer:
+                    KillerUserImage.Source = new BitmapImage(new Uri("/Images/killers/johnpizer.png", UriKind.Relative));
+                    break;
+                case Killers.John_Smith:
+                    KillerUserImage.Source = new BitmapImage(new Uri("/Images/killers/johnsmith.png", UriKind.Relative));
+                    break;
+                case Killers.Joseph_Lane:
+                    KillerUserImage.Source = new BitmapImage(new Uri("/Images/killers/josephlane.png", UriKind.Relative));
+                    break;
+                case Killers.Madame:
+                    KillerUserImage.Source = new BitmapImage(new Uri("/Images/killers/madame.png", UriKind.Relative));
+                    break;
+                case Killers.Miss_Stealthy:
+                    KillerUserImage.Source = new BitmapImage(new Uri("/Images/killers/missstealthy.png", UriKind.Relative));
+                    break;
+                case Killers.Sgt_Goodley:
+                    KillerUserImage.Source = new BitmapImage(new Uri("/Images/killers/sgtgoodley.png", UriKind.Relative));
+                    break;
+                case Killers.William_Gull:
+                    KillerUserImage.Source = new BitmapImage(new Uri("/Images/killers/williamgull.png", UriKind.Relative));
+                    break;
+                // TODO : Add other enumeration cases
+                default:
+                    break;
+            }
+
+            // Points :
+            PointSherlock.Text = currentGame.DetectivePoints.ToString();
+            PointMrJack.Text = currentGame.KillerPoints.ToString();
+
+            // Board
+            for (int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j< 5; j++)
+                {
+                    cards[i, j].SetCard(currentGame.GameBoard.Board[i, j]);
+                }
+            }
+        }
+
+        #endregion
+
+        #region UI Helpers
+
+        private Button GetAvailableActionButton(int i)
+        {
+            switch(i)
+            {
+                case 1:
+                    return AvailableAction1Btn;
+                case 2:
+                    return AvailableAction2Btn;
+                case 3:
+                    return AvailableAction3Btn;
+                case 4:
+                    return AvailableAction4Btn;
+                default:
+                    return null;
+            }
+        }
+
+        private Image GetAvailableActionImage(int i)
+        {
+            switch (i)
+            {
+                case 1:
+                    return AvailableAction1Img;
+                case 2:
+                    return AvailableAction2Img;
+                case 3:
+                    return AvailableAction3Img;
+                case 4:
+                    return AvailableAction4Img;
+                default:
+                    return null;
+            }
+        }
+
+        #endregion
     }
 }
