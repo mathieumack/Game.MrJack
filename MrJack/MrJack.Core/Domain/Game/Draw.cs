82263fbe930e8ee6a63c38815bceb6791f442ac7
Elsa <elsa.demaine@gmail.com>
2018-04-04 11:11:03 +0200

    [add] Dictionary <killers, sabliers>

--- MrJack/MrJack.Core/Domain/Game/Draw.cs
+++ MrJack/MrJack.Core/Domain/Game/Draw.cs
@@ -9,6 +9,7 @@ namespace MrJack.Core.Domain.Game
     class Draw
     {
         public List<Killers> Cartes { get; set; }
+        public Dictionary<Killers, int> killersSabliers { get; set;}
 
         /// <summary>
         /// Contructeur de la pioche
@@ -24,17 +25,33 @@ namespace MrJack.Core.Domain.Game
             Cartes.Add(Killers.Sgt_Goodley);
             Cartes.Add(Killers.Madame);
             Cartes.Add(Killers.John_Pizzer);
+
+            killersSabliers.Add(Killers.William_Gull, 1);
+            killersSabliers.Add(Killers.John_Smith, 1);
+            killersSabliers.Add(Killers.Jeremy_Bert, 1);
+            killersSabliers.Add(Killers.Joseph_Lane, 1);
+            killersSabliers.Add(Killers.Miss_Stealthy, 1);
+            killersSabliers.Add(Killers.Insp_Lestrade, 0);
+            killersSabliers.Add(Killers.Sgt_Goodley, 0);
+            killersSabliers.Add(Killers.Madame, 2);
+            killersSabliers.Add(Killers.John_Pizzer, 1);
         }
 
         /// <summary>
-        /// Permet de piocher un carte
+        /// Permet de piocher un carte (et ajouter des sabliers si MrJack)
         /// </summary>
+        /// <param name="currentPlayer">Le joueur en cours</param>
         /// <returns>le tueur sur la carte</returns>
-        public Killers Pioche()
+        public Killers Pioche(PlayerType currentPlayer)
         {
             Random rnd = new Random();
             int nb = rnd.Next(0, 10);
             Killers killer = Cartes[nb];
+            if(currentPlayer == PlayerType.MrJack)
+            {
+                killersSabliers.TryGetValue(killer, out int sabliers);
+                Joueur.Sabliers += sabliers;
+            }
             Cartes.Remove(killer);
             return killer;
         }
