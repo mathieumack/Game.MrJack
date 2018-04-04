88f54b6b82fbb44fef28c25ffedf75fcc839d7bd
Hugo Lausenaz-Pire <hugo.lausenazpire@epsi.fr>
2018-04-04 14:21:14 +0200

    Revert "Merge remote-tracking branch 'origin/TokenAction' into Game"
    This reverts commit 97f0617195d4282d6ddd938ea660cc5639914286.

--- MrJack/MrJack.Core/Domain/Game/TokenAction.cs
+++ MrJack/MrJack.Core/Domain/Game/TokenAction.cs
@@ -6,24 +6,15 @@ using System.Threading.Tasks;
 
 namespace MrJack.Core.Domain.Game
 {
-    public class TokenAction
+    class TokenAction
     {
-        public Token Token1 { get ; set ;}
-        public Token Token2 { get; set; }
-        public Token Token3 { get; set; }
-        public Token Token4 { get; set; }
+        public ActionType jeton1 {get ; set ;}
+        public ActionType jeton2 { get; set; }
+        public ActionType jeton3 { get; set; }
+        public ActionType jeton4 { get; set; }
 
         Random rnd = new Random();
         
-        public TokenAction()
-        {
-            Token1 = new Token(ActionType.Joker);
-            Token2 = new Token(ActionType.Move);
-            Token3 = new Token(ActionType.Draw);
-            Token4 = new Token(ActionType.Toby);
-            Lancer();
-        }
-
         /// <summary>
         /// If pour savoir quel jeton a quelle valeur
         /// </summary>
@@ -32,38 +23,37 @@ namespace MrJack.Core.Domain.Game
             int nb1 = rnd.Next(1, 3);
             if (nb1 == 1)
             {
-                Token1.ActionType =  ActionType.Joker;
-            }
-            else
+                jeton1 = ActionType.Joker;
+            } else
             {
-                Token1.ActionType = ActionType.Turn;
+                jeton1 = ActionType.Turn;
             }
             int nb2 = rnd.Next(1, 3);
-            if (nb2 == 1)
+            if (nb2 == 2)
             {
-                Token2.ActionType = ActionType.Move;
+                jeton2 = ActionType.Move;
             }
             else
             {
-                Token2.ActionType = ActionType.Turn;
+                jeton2 = ActionType.Turn;
             }
             int nb3 = rnd.Next(1, 3);
-            if (nb3 == 1)
+            if (nb3 == 3)
             {
-                Token3.ActionType = ActionType.Draw;
+                jeton3 = ActionType.Draw;
             }
             else
             {
-                Token3.ActionType = ActionType.Sherlock;
+                jeton3 = ActionType.Sherlock;
             }
             int nb4 = rnd.Next(1, 3);
-            if (nb4 == 1)
+            if (nb4 == 4)
             {
-                Token4.ActionType = ActionType.Toby;
+                jeton4 = ActionType.Toby;
             }
             else
             {
-                Token4.ActionType = ActionType.Watson;
+                jeton4 = ActionType.Watson;
             }
         } 
         /// <summary>
@@ -71,41 +61,42 @@ namespace MrJack.Core.Domain.Game
         /// </summary>
         public void Tourner()
         {
-            if (Token1.ActionType == ActionType.Joker)
+            if (jeton1 == ActionType.Joker)
             {
-                Token1.ActionType = ActionType.Turn;
+                jeton1 = ActionType.Turn;
             }
             else
             {
-                Token1.ActionType = ActionType.Joker;
+                jeton1 = ActionType.Joker;
             }
 
-            if (Token2.ActionType == ActionType.Move)
+            if (jeton2 == ActionType.Move)
             {
-                Token2.ActionType = ActionType.Turn;
+                jeton2 = ActionType.Turn;
             }
             else
             {
-                Token2.ActionType = ActionType.Move;
+                jeton2 = ActionType.Move;
             }
 
-            if (Token3.ActionType == ActionType.Draw)
+            if (jeton3 == ActionType.Draw)
             {
-                Token3.ActionType = ActionType.Sherlock;
+                jeton3 = ActionType.Sherlock;
             }
             else
             {
-                Token3.ActionType = ActionType.Draw;
+                jeton3 = ActionType.Draw;
             }
 
-            if (Token4.ActionType == ActionType.Toby)
+            if (jeton4 == ActionType.Toby)
             {
-                Token4.ActionType = ActionType.Watson;
+                jeton4 = ActionType.Watson;
             }
             else
             {
-                Token4.ActionType = ActionType.Toby;
+                jeton4 = ActionType.Toby;
             }
-        } 
+        }
+                
     }
 }
