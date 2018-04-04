7c2401a0d734339f50d72cf5c8710fc85f575c37
Rabatel <loris.rabatel@epsi.fr>
2018-04-04 10:30:53 +0200

    Draw [add]

---
+++ MrJack/MrJack.Core/Domain/Game/Draw.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrJack.Core.Domain.Game
+{
+    class Draw
+    {
+        public List<Killers> Cartes { get; set; }
+        
+        public void List()
+        {
+            Cartes.Add(Killers.William_Gull);
+            Cartes.Add(Killers.John_Smith);
+            Cartes.Add(Killers.Jeremy_Bert);
+            Cartes.Add(Killers.Joseph_Lane);
+            Cartes.Add(Killers.Miss_Stealthy);
+            Cartes.Add(Killers.Insp_Lestrade);
+            Cartes.Add(Killers.Sgt_Goodley);
+            Cartes.Add(Killers.Madame);
+            Cartes.Add(Killers.John_Pizzer);
+        }
+    }
+}
