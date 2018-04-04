6632a756f7dcfef3a0b33079aab8b5af203bcbb8
Elsa <elsa.demaine@gmail.com>
2018-04-04 14:39:22 +0200

    [add] Class Token [mod] TokenAction [add] Class Test_TokenAction

---
+++ MrJack/MrJack.Core/Domain/Game/Token.cs
@@ -0,0 +1,21 @@
+﻿using MrJack.Core.Interfaces.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrJack.Core.Domain.Game
+{
+    class Token : IAction
+    {
+        public ActionType ActionType { get; set; }
+        public bool Selectable { get; set; }
+
+        public Token(ActionType actionType)
+        {
+            ActionType = actionType;
+            Selectable = true;
+        }
+    }
+}
