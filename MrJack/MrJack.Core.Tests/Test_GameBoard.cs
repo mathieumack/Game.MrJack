32838d3e0dd8abadd593a304f6fcd9c0553d7b70
Elsa <elsa.demaine@gmail.com>
2018-04-04 16:54:56 +0200

    en cours

--- MrJack/MrJack.Core.Tests/Test_GameBoard.cs
+++ MrJack/MrJack.Core.Tests/Test_GameBoard.cs
@@ -13,14 +13,18 @@ namespace MrJack.Core.Tests
         public void Test_GameBoard_Init()
         {
             GameBoard Gb = new GameBoard();
+            int nb = 0;
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 1; i < 3; i++)
             {
-                for (int j = 0; j < 5; j++)
+                for (int j = 1; j < 3; j++)
                 {
-                    Gb.Board[i,j]
+                    Assert.IsTrue(Gb.Board[i, j].CardType == CardType.Card);
+                    nb++;
                 }
             }
+
+            Assert.IsTrue(nb == 9);
 
             Assert.IsTrue(Gb.Board[0, 1].Detective == Detectives.Sherlock);
             Assert.IsTrue(Gb.Board[4, 1].Detective == Detectives.Watson);
