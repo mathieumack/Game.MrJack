7e3ba72e6394749fedce4f4996e88bfcf966a92c
Rabatel <loris.rabatel@epsi.fr>
2018-04-11 10:41:32 +0200

    ok

---
+++ MrJack/MrJack.Core.Tests/TokenActionTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MrJack.Core.Tests
+{
+    [TestClass]
+    public class TokenActionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(TokenAction.lancer());
+        }
+    }
+}
