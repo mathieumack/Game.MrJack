355c09f3b9013f7cfa684f0dec18784b39fe9e8a
Elsa <elsa.demaine@gmail.com>
2018-04-18 08:46:26 +0200

    [mod] finish Mr Jack Easy

--- MrJack/MrJack.Core/Domain/Game/AI_MrJack_Easy.cs
+++ MrJack/MrJack.Core/Domain/Game/AI_MrJack_Easy.cs
@@ -12,7 +12,7 @@ namespace MrJack.Core.Domain.Game
         public Killers Killer { get; set; }
         public Random Rnd { get; set; }
         public IGame Game { get; set; }
-        public GameBoard GB { get; set; }
+        public IGameBoard GB { get; set; }
 
         public AI_MrJack_Easy(Killers killer, Random rnd, IGame game) : base(PlayerType.MrJack)
         {
@@ -21,7 +21,10 @@ namespace MrJack.Core.Domain.Game
             Game = game;
             GB = Game.GameBoard;
         }
-          
+        
+        /// <summary>
+        /// Allows Mr Jack to do an action
+        /// </summary>
         public void ChooseAction()
         {
             for(int actionIndex = 0; actionIndex < Game.AvailableActions.Count; actionIndex++)
@@ -30,14 +33,15 @@ namespace MrJack.Core.Domain.Game
                 {
                     if(Game.AvailableActions[actionIndex].ActionType == ActionType.Draw)
                     {
-
-                    } else if (Game.AvailableActions[actionIndex].ActionType == ActionType.Joker)
+                        Draw(actionIndex);
+                    }
+                    else if (Game.AvailableActions[actionIndex].ActionType == ActionType.Joker)
                     {
-
+                        Joker(actionIndex);
                     }
                     else if (Game.AvailableActions[actionIndex].ActionType == ActionType.Move)
                     {
-
+                        Move(actionIndex);
                     }
                     else if (Game.AvailableActions[actionIndex].ActionType == ActionType.Sherlock)
                     {
@@ -45,26 +49,25 @@ namespace MrJack.Core.Domain.Game
                     }
                     else if (Game.AvailableActions[actionIndex].ActionType == ActionType.Toby)
                     {
-
+                        Toby(actionIndex);
                     }
                     else if (Game.AvailableActions[actionIndex].ActionType == ActionType.Turn)
                     {
-
+                        Turn(actionIndex);
                     }
                     else if (Game.AvailableActions[actionIndex].ActionType == ActionType.Watson)
                     {
-
+                        Watson(actionIndex);
                     }
                 }
-
-            }
-
-                                
-                                
+            }                                            
         }
 
 
-        
+        /// <summary>
+        /// Moves the Sherlock token
+        /// </summary>
+        /// <param name="actionIndex">token number</param>
         public void Sherlock(int actionIndex)
         {
             int nb = Rnd.Next(1, 3);
@@ -74,52 +77,116 @@ namespace MrJack.Core.Domain.Game
                 {
                     if(GB.Board[i,j].Detective == Detectives.Sherlock)
                     {
-                        Game.MoveDetective(i,j,nb,);
-                        Game.MoveDetective()
+                        Game.MoveDetective(actionIndex, i, j, nb);
+                    }
+                }
+            }           
+        }
+
+        /// <summary>
+        /// Moves the Watson token
+        /// </summary>
+        /// <param name="actionIndex">token number</param>
+        public void Watson(int actionIndex)
+        {
+            int nb = Rnd.Next(1, 3);
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j <= 5; j++)
+                {
+                    if (GB.Board[i, j].Detective == Detectives.Watson)
+                    {
+                        Game.MoveDetective(actionIndex, i, j, nb);
                     }
                 }
             }
-           
         }
 
-        public void Watson()
+        /// <summary>
+        /// Moves the Toby token
+        /// </summary>
+        /// <param name="actionIndex">token number</param>
+        public void Toby(int actionIndex)
         {
             int nb = Rnd.Next(1, 3);
-            Game.Watson(nb);
-        }
-
-        public void Toby()
-        {
-            int nb = Rnd.Next(1, 3);
-            Game.Toby(nb);
-        }
-
-        public void Joker()
-        {
-            int nb = Rnd.Next(0, 2);
-            Game.Joker(nb);
-        }
-
-        public void Draw()
-        {
-            Game.Draw();
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j <= 5; j++)
+                {
+                    if (GB.Board[i, j].Detective == Detectives.Toby)
+                    {
+                        Game.MoveDetective(actionIndex, i, j, nb);
+                    }
+                }
+            }
         }
         
-        public void Move()
+        /// <summary>
+        /// Moves the any token
+        /// </summary>
+        /// <param name="actionIndex">token number</param>
+        public void Joker(int actionIndex)
+        {
+            Detectives joker;
+            int nbjeton = Rnd.Next(1, 3);
+            if(nbjeton == 1)
+            {
+                joker = Detectives.Sherlock;
+            }
+            else if (nbjeton == 2)
+            {
+                joker = Detectives.Watson;
+            }
+            else
+            {
+                joker = Detectives.Toby;
+            }
+
+            int nb = Rnd.Next(0, 2);
+            for (int i = 0; i <= 5; i++)
+            {
+                for (int j = 0; j <= 5; j++)
+                {
+                    if (GB.Board[i, j].Detective == joker)
+                    {
+                        Game.MoveDetective(actionIndex, i, j, nb);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Draws a card
+        /// </summary>
+        /// <param name="actionIndex">token number</param>
+        public void Draw(int actionIndex)
+        {
+            Game.Draw(actionIndex);
+        }
+        
+        /// <summary>
+        /// Moves the position of two cards
+        /// </summary>
+        /// <param name="actionIndex">token number</param>
+        public void Move(int actionIndex)
         {
             int x1 = Rnd.Next(1, 4);
             int y1 = Rnd.Next(1, 4);
             int x2 = Rnd.Next(1, 4);
             int y2 = Rnd.Next(1, 4);
-            Game.MoveCard(x1, y1, x2, y2);
+            Game.MoveCard(actionIndex,x1, y1, x2, y2);
         }
-
-        public void Turn()
+        
+        /// <summary>
+        /// Turns the position of a card
+        /// </summary>
+        /// <param name="actionIndex">token number</param>
+        public void Turn(int actionIndex)
         {
             int x = Rnd.Next(1, 4);
             int y = Rnd.Next(1, 4);
             int nb = Rnd.Next(1, 4);
-            Game.TurnCard(x, y, nb);
+            Game.TurnCard(actionIndex,x, y, nb);
         }
 
     }
