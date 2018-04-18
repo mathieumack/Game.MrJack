ed22c0a9cb36ef2d6ac5d68eb56394b3adfe918d
Rabatel <loris.rabatel@epsi.fr>
2018-04-18 11:25:12 +0200

    [mod]

--- MrJack/MrJack.Core/Domain/Game/AI_Sherlock.cs
+++ MrJack/MrJack.Core/Domain/Game/AI_Sherlock.cs
@@ -71,6 +71,189 @@ namespace MrJack.Core.Domain.Game
         public void Sherlock(int actionIndex)
         {
             int nb = Rnd.Next(1, 3);
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (GB.Board[i, j].Detective == Detectives.Sherlock)
+                    {
+                        Game.MoveDetective(actionIndex, i, j, nb);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Moves the Watson token
+        /// </summary>
+        /// <param name="actionIndex">token number</param>
+        public void Watson(int actionIndex)
+        {
+            int nb = Rnd.Next(1, 3);
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (GB.Board[i, j].Detective == Detectives.Watson)
+                    {
+                        Game.MoveDetective(actionIndex, i, j, nb);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Moves the Toby token
+        /// </summary>
+        /// <param name="actionIndex">token number</param>
+        public void Toby(int actionIndex)
+        {
+            int nb = Rnd.Next(1, 3);
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (GB.Board[i, j].Detective == Detectives.Toby)
+                    {
+                        Game.MoveDetective(actionIndex, i, j, nb);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Moves the any token
+        /// </summary>
+        /// <param name="actionIndex">token number</param>
+        public void Joker(int actionIndex)
+        {
+            Detectives joker;
+            int nbjeton = Rnd.Next(1, 3);
+            if (nbjeton == 1)
+            {
+                joker = Detectives.Sherlock;
+            }
+            else if (nbjeton == 2)
+            {
+                joker = Detectives.Watson;
+            }
+            else
+            {
+                joker = Detectives.Toby;
+            }
+
+            int nb = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (GB.Board[i, j].Detective == joker)
+                    {
+                        Game.MoveDetective(actionIndex, i, j, nb);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Draws a card
+        /// </summary>
+        /// <param name="actionIndex">token number</param>
+        public void Draw(int actionIndex)
+        {
+            Game.Draw(actionIndex);
+        }
+
+        /// <summary>
+        /// Moves the position of two cards
+        /// </summary>
+        /// <param name="actionIndex">token number</param>
+        public void Move(int actionIndex)
+        {
+            int x1 = Rnd.Next(1, 4);
+            int y1 = Rnd.Next(1, 4);
+            int x2 = Rnd.Next(1, 4);
+            int y2 = Rnd.Next(1, 4);
+            Game.MoveCard(actionIndex, x1, y1, x2, y2);
+        }
+
+        /// <summary>
+        /// Turns the position of a card
+        /// </summary>
+        /// <param name="actionIndex">token number</param>
+        public void Turn(int actionIndex)
+        {
+            int x = Rnd.Next(1, 4);
+            int y = Rnd.Next(1, 4);
+            int nb = Rnd.Next(1, 4);
+            Game.TurnCard(actionIndex, x, y, nb);
+        }
+
+    }
+    public class AI_Detectives_Medium : Player
+    {
+        public Detectives Detectives { get; set; }
+        public Random Rnd { get; set; }
+        public IGame Game { get; set; }
+        public IGameBoard GB { get; set; }
+
+        public AI_Detectives_Medium(Detectives detectives, Random rnd, IGame game) : base(PlayerType.Sherlock)
+        {
+            detectives = Detectives;
+            Rnd = rnd;
+            Game = game;
+            GB = Game.GameBoard;
+        }
+
+        /// <summary>
+        /// Allows Mr Jack to do an action
+        /// </summary>
+        public void ChooseActionSherlock()
+        {
+            for (int actionIndex = 0; actionIndex < Game.AvailableActions.Count; actionIndex++)
+            {
+                if (Game.AvailableActions[actionIndex].Selectable)
+                {
+                    if (Game.AvailableActions[actionIndex].ActionType == ActionType.Draw)
+                    {
+                        Draw(actionIndex);
+                    }
+                    else if (Game.AvailableActions[actionIndex].ActionType == ActionType.Joker)
+                    {
+                        Joker(actionIndex);
+                    }
+                    else if (Game.AvailableActions[actionIndex].ActionType == ActionType.Move)
+                    {
+                        Move(actionIndex);
+                    }
+                    else if (Game.AvailableActions[actionIndex].ActionType == ActionType.Sherlock)
+                    {
+                        Sherlock(actionIndex);
+                    }
+                    else if (Game.AvailableActions[actionIndex].ActionType == ActionType.Toby)
+                    {
+                        Toby(actionIndex);
+                    }
+                    else if (Game.AvailableActions[actionIndex].ActionType == ActionType.Turn)
+                    {
+                        Turn(actionIndex);
+                    }
+                    else if (Game.AvailableActions[actionIndex].ActionType == ActionType.Watson)
+                    {
+                        Watson(actionIndex);
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Moves the Sherlock token
+        /// </summary>
+        /// <param name="actionIndex">token number</param>
+        public void Sherlock(int actionIndex)
+        {
+            int nb = Rnd.Next(1, 3);
             for (int i = 0; i <= 5; i++)
             {
                 for (int j = 0; j <= 5; j++)
