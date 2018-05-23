4536a570bc818b1ae0423b58f6f3714a82cc2127
Elsa <elsa.demaine@gmail.com>
2018-05-23 08:46:21 +0200

    [mod] fix joker Sherlock IA

--- MrJack/MrJack.Core/Domain/Game/AI_Sherlock.cs
+++ MrJack/MrJack.Core/Domain/Game/AI_Sherlock.cs
@@ -12,6 +12,42 @@ namespace MrJack.Core.Domain.Game
        
         public AI_Sherlock(Randomizer rnd, IGame game) : base(rnd, game, PlayerType.Sherlock)
         { }
+
+        public override void Joker(int actionIndex)
+        {
+            Detectives joker;
+            bool moved = false;
+            do
+            {
+                int nbjeton = Rnd.Next(1, 4);
+                if (nbjeton == 1)
+                {
+                    joker = Detectives.Sherlock;
+                }
+                else if (nbjeton == 2)
+                {
+                    joker = Detectives.Watson;
+                }
+                else
+                {
+                    joker = Detectives.Toby;
+                }
+
+                int nb = 1;
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (GB.Board[i, j].Detective == joker && GB.Board[i, j].CanBeMoved)
+                        {
+                            Game.MoveDetective(actionIndex, i, j, nb);
+                            moved = true;
+                        }
+                    }
+                }
+            }
+            while (!moved);
+        }
     }
 }
 
--- MrJack/MrJack.Core/Domain/Game/AI_Sherlock_Medium.cs
+++ MrJack/MrJack.Core/Domain/Game/AI_Sherlock_Medium.cs
@@ -24,5 +24,41 @@ namespace MrJack.Core.Domain.Game
                 ActionType.Draw,
             };
         }
+
+        public override void Joker(int actionIndex)
+        {
+            Detectives joker;
+            bool moved = false;
+            do
+            {
+                int nbjeton = Rnd.Next(1, 4);
+                if (nbjeton == 1)
+                {
+                    joker = Detectives.Sherlock;
+                }
+                else if (nbjeton == 2)
+                {
+                    joker = Detectives.Watson;
+                }
+                else
+                {
+                    joker = Detectives.Toby;
+                }
+
+                int nb = 1;
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (GB.Board[i, j].Detective == joker && GB.Board[i, j].CanBeMoved)
+                        {
+                            Game.MoveDetective(actionIndex, i, j, nb);
+                            moved = true;
+                        }
+                    }
+                }
+            }
+            while (!moved);
+        }
     }
 }
