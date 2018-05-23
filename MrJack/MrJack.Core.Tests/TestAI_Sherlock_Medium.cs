e3a633d91436c1d3b7b2ae7c427bea32b4a9b8f5
Rabatel <loris.rabatel@epsi.fr>
2018-05-23 09:06:47 +0200

    [Add] Test Ai_Sherlock_Medium

---
+++ MrJack/MrJack.Core.Tests/TestAI_Sherlock_Medium.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MrJack.Core.Domain.Game;
+using MrJack.Core.Interfaces.Game;
+
+namespace MrJack.Core.Tests
+{
+    [TestClass]
+    public class TestAI_Sherlock_Medium
+    {
+        
+    }
+}
